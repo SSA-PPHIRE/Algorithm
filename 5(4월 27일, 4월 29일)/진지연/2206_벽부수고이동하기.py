11c302e6a0ab4876237517792cdfbe5ce054af21
JinJiyeon <62229356+JinJiyeon@users.noreply.github.com>
2021-04-27 21:06:01 +0900

    Update 2206_벽부수고이동하기.py

--- 5(4월 27일, 4월 29일)/진지연/2206_벽부수고이동하기.py
+++ 5(4월 27일, 4월 29일)/진지연/2206_벽부수고이동하기.py
@@ -62,3 +62,55 @@ if -1 not in tmp:
     print(min(tmp))
 else:
     print(max(tmp))
+
+######################################################
+[참고] [오답]
+
+from collections import deque
+
+dr = [-1,1,0,0]
+dc = [0,0,-1,1]
+
+row, col = map(int, input().split())
+arr = [[*input()] for i in range(row)]
+
+# arr = [['0', '1', '1', '1'], ['1', '1', '1', '1'], ['1', '1', '1', '1'], ['1', '1', '1', '0']]
+# arr = [['0', '1', '0', '0'], ['1', '1', '1', '0'], ['1', '0', '0', '0'], ['0', '0', '0', '0'], ['0', '1', '1', '1'], ['0', '0', '0', '0']]
+
+
+visited = [[0]*col for i in range(row)]
+root_r, root_c, root_cnt, root_broke = 0,0,1,0
+ans = -1
+
+queue = deque()
+queue.append([root_r, root_c, root_cnt, root_broke])
+
+
+while queue:
+    r, c, cnt, broke = queue.popleft()
+    visited[r][c] = 1
+    # print(r,c,arr[r][c],cnt,broke,ans)
+
+    # 종료조건
+    if r == row-1 and c == col-1:
+        ans = cnt
+        break
+
+
+    for i in range(4):
+        new_r = r + dr[i]
+        new_c = c + dc[i]
+
+        if 0 > new_r or new_r >= row or 0 > new_c or new_c >= col:
+            continue
+
+        if visited[new_r][new_c] == 1:
+            continue
+
+        if arr[new_r][new_c] == '1' and broke == 0:
+            queue.append([new_r, new_c, cnt+1, 1])
+
+        elif arr[new_r][new_c] == '0':
+            queue.append([new_r, new_c, cnt+1, broke])
+
+print(ans)
