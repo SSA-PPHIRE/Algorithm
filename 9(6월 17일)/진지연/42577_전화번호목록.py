1f1e459addcf300d7615aaf61ad33812632a346c
JinJiyeon <62229356+JinJiyeon@users.noreply.github.com>
2021-06-15 14:14:45 +0900

    Create 42577_전화번호목록.py

---
+++ 9(6월 17일)/진지연/42577_전화번호목록.py
@@ -0,0 +1,19 @@
+# https://programmers.co.kr/learn/courses/30/lessons/42577
+
+# 문자열을 정렬하면 어떻게 되는지 주목
+# 효율성 통고 못함
+import re
+def solution(ipt):
+    ipt.sort()
+    for i in range(len(ipt)-1):
+        if re.match(ipt[i], ipt[i+1]):
+            return False
+    return True
+
+# 다르 사람 풀이
+def solution(phone_book):
+    phone_book.sort()
+    for p1,p2 in zip(phone_book,phone_book[1:]):
+        if p2.startswith(p1):
+            return False
+    return True
