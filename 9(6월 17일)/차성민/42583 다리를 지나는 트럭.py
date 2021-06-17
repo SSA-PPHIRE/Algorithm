77d20bbc98c5351f188acffca6a58b0e27a367c0
CAPCHA519 <ckpow9@gmail.com>
2021-06-17 17:02:33 +0900

    Update 42583 다리를 지나는 트럭.py

--- 9(6월 17일)/차성민/42583 다리를 지나는 트럭.py
+++ 9(6월 17일)/차성민/42583 다리를 지나는 트럭.py
@@ -4,9 +4,7 @@ def solution(bridge_length, weight, truck_weights):
     answer = 0
     queue = deque([0] * bridge_length) # 올라갈 수 있는 트럭 수 만큼의 크기로 큐 생성(트럭이 시간에 따라 지날 때 반영을 해주기 위해)
     current_weight = 0 # 현재까지의 무게 확인을 위해 생성해줌 (두개이상의 트럭도 같이 보낼 수 있게)
-
-    truck_weights.reverse() # 큰 값이 앞에 오도록
-
+    
     '''
         현재까지의 무게 + 가장 가벼운 트럭이 한계 무게보다 큰지 확인
         트럭을 올리는 방법만을 고려하여 시간을 먼저 구하고, 
