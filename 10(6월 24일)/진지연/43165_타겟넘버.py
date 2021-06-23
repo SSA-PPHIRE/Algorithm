a6079241a4b5ec2f05c0ded0ff075c59251c782f
JinJiyeon <62229356+JinJiyeon@users.noreply.github.com>
2021-06-23 14:26:27 +0900

    Create 43165_타겟넘버.py

---
+++ 10(6월 24일)/진지연/43165_타겟넘버.py
@@ -0,0 +1,18 @@
+# 프로그래머스에서도 함수 밖에 선언해도 되더이당~
+# cnt는 함수 밖에 선언해서 global로
+# numbers, target은 계속 받아와야 하므로 인자로 넣었다
+
+
+cnt = 0
+def dfs(numbers, target, i, pre_sum):
+    global cnt
+    if i == len(numbers):
+        if pre_sum == target:
+            cnt += 1
+        return
+    dfs(numbers, target, i + 1, pre_sum + numbers[i])
+    dfs(numbers, target, i + 1, pre_sum - numbers[i])
+
+def solution(numbers, target):
+    dfs(numbers, target, 0,0)
+    return cnt
