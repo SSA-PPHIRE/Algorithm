80ca3e3863dcaf7119b3ca11d65c37ddefc58fbd
JinJiyeon <62229356+JinJiyeon@users.noreply.github.com>
2021-11-01 19:11:47 +0900

    Update 최소비용.py

--- 23(11월 04일)/진지연/최소비용.py
+++ 23(11월 04일)/진지연/최소비용.py
@@ -38,3 +38,41 @@ for tc in range(1, t+1):
     dirs = [[0,1], [1,0], [0,-1], [-1,0]]
     dfs(0,0,0)
     print(f"#{tc} {ans}")
+
+    
+'''
+bfs로.
+더 좋은 값을 찾으면 다시 탐색한다
+'''
+
+from collections import deque
+delta = [[-1,0], [0,1], [1,0], [0,-1]]
+
+t = int(input())
+for tc in range(1, t+1):
+    n = int(input())
+    arr = [list(map(int, input().split())) for _ in range(n)]
+
+    visited = [[float('inf')]*n for _ in range(n)]
+    visited[0][0] = 0
+
+    q = deque([[0,0]])
+    while q:
+        r, c = q.popleft()
+
+        for i in range(4):
+
+            new_r = r + delta[i][0]
+            new_c = c + delta[i][1]
+
+            if new_r < 0 or new_r >= n or new_c < 0 or new_c >= n:
+                continue
+            diff = max(arr[new_r][new_c]-arr[r][c], 0)
+            
+            # 더 좋은 값을 찾으면 new_r, new_c를 다시 탐색한다
+            if visited[new_r][new_c] > visited[r][c] + 1 + diff:
+                visited[new_r][new_c] = visited[r][c] + 1 + diff
+                q.append([new_r, new_c])
+
+
+    print(f'#{tc} {visited[n-1][n-1]}')
