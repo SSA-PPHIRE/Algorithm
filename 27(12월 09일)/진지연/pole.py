56174252b5838c0a270a4042a963330c90fc4bef
JinJiyeon <62229356+JinJiyeon@users.noreply.github.com>
2021-12-07 10:19:04 +0900

    Update pole.py

--- 27(12월 09일)/진지연/pole.py
+++ 27(12월 09일)/진지연/pole.py
@@ -23,3 +23,48 @@ for tc in range(1, t+1):
         if countLook(i, L) & countLook(i[::-1], R):
             ans += 1
     print(f"#{tc} {ans}")
+    
+    
+'''
+시간초과 뜸
+'''
+
+def countLook(lst, limit):
+    mx = 0
+    tmp = 0
+    for l in lst:
+        if tmp > limit:
+            return (False, 0)
+        if l > mx:
+            mx = l
+            tmp += 1
+    return (True, tmp)
+
+
+def p(lst, N, L, R):
+    global cnt
+    print(lst)
+    if not countLook(lst, L)[0]:
+        return
+
+
+    if len(lst) == N:
+        if countLook(lst, L)[1] == L and countLook(lst[::-1], R)[1] == R:
+            cnt += 1
+        return
+
+    if len(lst) == N:
+        return
+
+    for i in range(1, N+1):
+        if i not in lst:
+            lst.append(i)
+            p(lst, N, L, R)
+            lst.remove(i)
+
+t = int(input())
+for tc in range(1, t+1):
+    N, L, R = map(int, input().split())
+    cnt = 0
+    p([], N, L, R)
+    print(f"#{tc} {cnt}")
